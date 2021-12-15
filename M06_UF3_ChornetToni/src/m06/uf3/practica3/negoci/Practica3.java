c6aee1e1bdea370dd9427eed87b9ac98ed6fce80
a20antchoand <a20antchoand@inspedralbes.cat>
2021-12-15 23:27:53 +0100

    Progres casi finalitzat

--- M06_UF3_ChornetToni/src/m06/uf3/practica3/negoci/Practica3.java
+++ M06_UF3_ChornetToni/src/m06/uf3/practica3/negoci/Practica3.java
@@ -1,5 +1,81 @@
-package m06.uf3.practica3.negoci;
+import org.basex.core.BaseXException;
+import org.basex.core.Context;
+import org.basex.core.cmd.CreateDB;
+import org.basex.core.cmd.Open;
+import org.basex.core.cmd.XQuery;
 
 public class Practica3 {
+
+    static Context context = new Context();
+    static Interficie interficie = new Interficie();
+    static String pais;
+
+    public static void main(String[] args) throws BaseXException {
+
+        interficie.Header();
+        interficie.Llistat();
+
+        comprovarBBDD("for $p in mondial return $p/country/name[1]/text()");
+
+
+        interficie.DemanaPais();
+        interficie.Resultat();
+
+        comprovarBBDD("//country/name='" + pais + "'");
+        comprovarBBDD("//country[@inflation]/name='" + pais + "'");
+        comprovarBBDD("//country[@name='" + pais + "']/religions/text()");
+
+    }
+
+    private static void comprovarBBDD(String query) {
+
+        try {
+
+            new Open("factbook").execute(context);
+
+            try {
+
+                query(query);
+
+            } catch (BaseXException e) {
+
+                System.out.println("Error en la XQUERY");
+
+            }
+
+        } catch (Exception e) {
+
+            try {
+
+                System.out.println("Create a new databae.");
+                new CreateDB("DBExemple", "universitats-v2.xml").execute(context);
+                System.out.println("S'ha creat la BD");
+
+            } catch (Exception ex) {
+
+                System.out.println("No se la creado la DB");
+            }
+
+        } finally {
+
+            try {
+
+                context.close();
+
+            } catch (Exception e) {
+
+                System.out.println("No se ha podido cerrar la DB");
+
+            }
+
+        }
+
+    }
+
+
+    static public void query(final String query) throws BaseXException {
+        System.out.println(new XQuery(query).execute(context));
+    }
+
 
 }
--- M06_UF3_ChornetToni/src/m06/uf3/practica3/presentacio/Interficie.java
+++ M06_UF3_ChornetToni/src/m06/uf3/practica3/presentacio/Interficie.java
@@ -1,5 +1,46 @@
-package m06.uf3.practica3.presentacio;
+import java.util.Scanner;
 
 public class Interficie {
+
+    static Scanner s = new Scanner(System.in);
+
+    public void Header () {
+
+        StringBuilder header = new StringBuilder();
+
+        header.append("####################################################################\n");
+        header.append("************PROGRAMA DE CONSULTA DEL FITXER factbook.xml************\n");
+        header.append("***************Versio 1. BD incrustada en el programa***************\n");
+        header.append("####################################################################\n");
+
+        System.out.println(header);
+
+    }
+
+    public void Llistat () {
+
+        String llistat = "************La llista actual de paisos es: ************\n";
+
+        System.out.println(llistat);
+
+    }
+
+    public void DemanaPais() {
+        System.out.print("***Siusplau introdueix un pais: ");
+        Practica3.pais = s.nextLine();
+    }
+
+    public void Resultat () {
+
+        StringBuilder resultat = new StringBuilder();
+
+        resultat.append("**************************************\n");
+        resultat.append("**************************************\n");
+        resultat.append("***Les dades del pais escollit son:***\n");
+
+        System.out.println(resultat);
+
+    }
+
 
 }
